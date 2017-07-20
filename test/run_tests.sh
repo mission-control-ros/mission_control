3622d57b90b3bc5e5e36bba2f0dbb0ebddfb15e9
elgar <elgar.lepp@gmail.com>
2017-07-20 16:14:21 +0300

    T54 Travis CI build to fail when tests actually fail

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
-rostest mission_control test_behaviour_token_on_startup.launch --text
-rostest mission_control test_behaviour_token_passing.launch --text
-rostest mission_control test_behaviour_token_passing2.launch --text
-rostest mission_control test_behaviour_variable_passing.launch --text
-rostest mission_control test_behaviour_subprocess_token_passing.launch --text
-rostest mission_control test_behaviour_subprocess_token_passing2.launch --text
-rostest mission_control test_behaviour_subprocess_token_passing_with_cpp_script.launch --text
-rostest mission_control test_watchdog.launch --text
-rostest mission_control test_watchdog_fail_safe.launch --text
-rostest mission_control test_watchdog_fail_safe_subprocess.launch --text
-rostest mission_control test_mission_control_utils_set_var.launch --text
-rostest mission_control test_mission_control_utils_get_var.launch --text
-rostest mission_control test_mission_control_utils_cpp_set_var.launch --text
-rostest mission_control test_mission_control_utils_cpp_get_var.launch --text
-rostest mission_control test_behaviour.launch --text
+
+rm -f ~/.ros/.coverage
+
+TESTS=(
+  test_behaviour_token_on_startup.launch
+  test_behaviour_token_passing.launch
+  test_behaviour_token_passing2.launch
+  test_behaviour_variable_passing.launch
+  test_behaviour_subprocess_token_passing.launch
+  test_behaviour_subprocess_token_passing2.launch
+  test_behaviour_subprocess_token_passing_with_cpp_script.launch
+  test_watchdog.launch
+  test_watchdog_fail_safe.launch
+  test_watchdog_fail_safe_subprocess.launch
+  test_mission_control_utils_set_var.launch
+  test_mission_control_utils_get_var.launch
+  test_mission_control_utils_cpp_set_var.launch
+  test_mission_control_utils_cpp_get_var.launch
+  test_behaviour.launch
+)
+
+EXIT_CODE=0
+
+for TEST in ${TESTS[@]}; do
+
+  CMD="rostest mission_control ${TEST}"
+  eval $CMD
+
+  TEST_RESULT=$?
+
+  if [ $TEST_RESULT != 0 ]; then
+    EXIT_CODE=1
+  fi
+
+done
+
+exit $EXIT_CODE
