034785fd7fa0fcaf0dad1172aa0d63bd0a24be4d
elgar <elgar.lepp@gmail.com>
2018-01-26 15:08:59 +0200

    Separate move_base launch and use_case launch

---
+++ examples/scripts/start_move_base.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source /catkin_ws/devel/setup.bash
+roscd mission_control/examples/scripts
+source set_env_variables.sh
+
+echo "Start xvfb..."
+Xvfb -shmem -screen 0 1280x1024x24 &
+sleep 2
+
+echo "Start gazebo server..."
+DISPLAY=:0 roslaunch mission_control use_case_world.launch gui:=false &
+echo "Wait for 2 minutes, because some models need to be downloaded..."
+sleep 120
+
+echo "Spawn model..."
+roslaunch mission_control use_case_robot.launch &
+echo "Wait for 5 seconds..."
+sleep 5
+
+echo "Start amcl..."
+roslaunch mission_control amcl.launch &
+echo "Wait for 10 seconds, to get the message 'odom received!'..."
+sleep 10
